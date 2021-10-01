00e0397fa2e064a417d8f5575b806797e9eb445b
Moaddat Naqvi <moaddatnaqvi@gmail.com>
2021-09-30 20:50:56 -0700

    part 4 task 3

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-"if __name__ == '__main__':                                                                                                x = "ECE_180_DA_DB"                                                                                                      if x == "EE_180_DA_DB":                                                                                                      print("You are living in 2017")                                                                                    else:                                                                                                                         # this is a comment                                                                                                   x = x + " - Best class ever"                                                                                            print(x)"
+"if __name__ == '__main__':" 
+"   x = "ECE_180_DA_DB":" 
+"   x = "ECE_180_DA_DB":" 
+"   x = "ECE_180_DA_DB":" 
